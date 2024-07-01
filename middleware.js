933516e27910e14d8819531d4055bbe32acfb1ec
sheraz4196 <sheraztoor321@gmail.com>
2024-07-01 22:41:40 +0500

    feat: add middleware

---
+++ middleware.js
@@ -0,0 +1,36 @@
+import { NextResponse } from "next/server";
+import { randomUUID } from "crypto"; // Import randomUUID directly from the crypto module
+
+export function middleware(request) {
+  // eslint-disable-next-line no-undef
+  const nonce = Buffer.from(randomUUID()).toString("base64");
+
+  const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
+    style-src 'self' 'nonce-${nonce}';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+  `
+    .replace(/\s{2,}/g, " ")
+    .trim();
+
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set("x-nonce", nonce);
+  requestHeaders.set("Content-Security-Policy", cspHeader);
+
+  const response = NextResponse.next({
+    request: {
+      headers: requestHeaders,
+    },
+  });
+
+  response.headers.set("Content-Security-Policy", cspHeader);
+
+  return response;
+}
